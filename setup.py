4fc2ab48fd027b67463cc3bfdc58aa4039778644
chukhranchick <ivan.chuhran@gmail.com>
2023-03-27 16:44:41 +0300

    add setup.py and requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='app',
+    version='',
+    packages=['app', 'polls', 'polls.migrations'],
+    url='',
+    license='',
+    author='chukhranchick',
+    author_email='',
+    description=''
+)
